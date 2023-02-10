e0ae966364b92389a8d2eda4e0029ba04c921520
Febin Mathew <febnmathew98@gmail.com>
2023-02-10 11:16:46 +0530

    Initial commit

---
+++ base/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Room, Messages, Topic
+
+
+admin.site.register(Room)
+admin.site.register(Messages)
+admin.site.register(Topic)
