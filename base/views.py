2c1aa6339910a74cae21914bc84445290dc2b216
Febin Mathew <febnmathew98@gmail.com>
2023-02-19 19:10:33 +0530

    finals

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.db.models import Q
 from .models import Room, Topic, Messages
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
@@ -25,7 +25,7 @@ def home(request):
 
     room_count = rooms.count()
     room_messages = Messages.objects.filter(Q(room__topic__name__icontains=q))
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     context = {"rooms":rooms, "topics":topics, "room_count":room_count, "room_messages": room_messages}
     
     return render(request,"base/home.html",context)
@@ -53,15 +53,24 @@ def room(request,pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
+    
     if request.method == "POST":
-        form =RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host= request.user
-            room.save()
-            return redirect('home')
+       topic_name = request.POST.get('topic')
+       topic,created = Topic.objects.get_or_create(name = topic_name)
 
-    context = {"form":form}
+       Room.objects.create(
+           host=request.user,
+           topic= topic,
+           name=request.POST.get('name'),
+           description=request.POST.get('description')
+       )
+       return redirect('home')
+
+
+    
+
+    context = {"form":form, "topics":topics}
     return render(request, 'base/room_form.html',context)
 
 
@@ -69,17 +78,22 @@ def createRoom(request):
 def updateRoom(request,pk):
     room = Room.objects.get(id =pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse("You are not allowed here")
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-           form.save()
-           return redirect('home')
+        
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
 
-    context = {"form":form}
+    context = {"form":form, "topics":topics, "room":room}
     return render(request, 'base/room_form.html', context)
 
 
@@ -179,3 +193,29 @@ def userProfile(request,pk):
     topics = Topic.objects.all()
     context = {'user':user, 'rooms':rooms, 'room_messages': room_messages, 'topics':topics}
     return render(request, 'base/profile.html',context)
+
+login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+      form = UserForm(request.POST, instance=user)
+      if form.is_valid():
+          form.save()
+          return redirect('user-profile', pk=user.id)
+
+    return render(request, 'base/update-user.html',{'form':form,"user":user})
+
+
+def topicPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains = q)
+
+    return render(request, 'base/topics.html', {"topics":topics})
+
+
+def activityPage(request):
+
+    room_messages = Messages.objects.all()
+    return render(request, 'base/activity.html', {'room_messages':room_messages})
--- studybud/settings.py
+++ studybud/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
+    
 ]
 
 ROOT_URLCONF = 'studybud.urls'
@@ -128,3 +134,8 @@ STATICFILES_DIRS =[
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+CORS_ALLOW_ALL_ORIGINS = True
